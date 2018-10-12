a592642c13a04ee82cc9471df37c8f51a9c7d4f2
GlassyWing <1490215053@qq.com>
2018-10-12 17:34:51 +0800

    加入图像缓存机制

---
+++ cyclegan/image_pool.py
@@ -0,0 +1,28 @@
+import keras.backend as K
+import numpy as np
+
+
+class ImagePool:
+
+    def __init__(self, pool_size=50):
+        self.images = []
+        self.pool_size = pool_size
+        self.num_imgs = 0
+        if pool_size < 0:
+            raise ValueError(f"Illegal pool size ${pool_size} which should be >= 0")
+
+    def query(self, images):
+        if self.pool_size == 0:
+            return images
+        return_images = []
+        for img in images:
+            img = K.expand_dims(img, axis=0)
+            if self.num_imgs < self.pool_size:
+                self.images.append(img)
+                self.num_imgs += 1
+                return_images.append(img)
+            else:
+                idx = np.random.randint(0, self.pool_size)
+                return_images.append(self.images[idx])
+                self.images[idx] = img
+        return K.concatenate(return_images, axis=0)
